b1a567601f07da6eb9f9f80bf607ff77f786e4a7
suraj <suraj200981@yahoo.co.uk>
2023-04-16 14:07:53 +0100

    Initial commit

---
+++ src/main/java/com/example/url/shortner/microservices/redirectionservice/RedirectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.url.shortner.microservices.redirectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectionServiceApplication.class, args);
+	}
+
+}
